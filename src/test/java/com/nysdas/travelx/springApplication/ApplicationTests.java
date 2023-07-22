575ca73b93cc3ec55331ee567c78847cb119db2a
nysdas <nysdas@icloud.com>
2023-07-22 11:03:30 -0400

    add starter files
    This commit has the starter files for the project.
    
    Dependencies included:
    	- MongoDB driver
    	- Spring Web
    	- ThymeLeaf

---
+++ src/test/java/com/nysdas/travelx/springApplication/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nysdas.travelx.springApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
